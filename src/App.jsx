4db5a06c5275b0255ea712694226cfbcbcb602cf
Parviz-S <parvizs@innovativecentre.org>
2023-10-06 18:31:10 +0500

    Linking the database with the frontend

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [animals, setAnimals] = useState([]);
+
+  const search = async (q) => {
+    const response = await fetch(
+      "http://localhost:8080?" + new URLSearchParams({ q })
+    );
+    const data = await response.json();
+    setAnimals(data);
+  };
 
   return (
-    <>
-      
-    </>
-  )
+    <main>
+      <h1>Animal Farm</h1>
+
+      <input
+        type="text"
+        placeholder="Search"
+        onChange={(e) => search(e.target.value)}
+      />
+
+      <ul>
+        {animals.map((animal) => (
+          <Animal key={animal.id} {...animal} />
+        ))}
+
+        {animals.length === 0 && "No animals found"}
+      </ul>
+    </main>
+  );
 }
 
-export default App
+function Animal({ type, name, age }) {
+  return (
+    <li>
+      <strong>{type}</strong> {name} ({age} years old)
+    </li>
+  );
+}
+
+export default App;
